41c7c9d8b6f1a02f5aa375918ce1630c308b290a
envy124 <aspidfire9664@gmail.com>
2017-04-29 19:33:14 +0300

    call active flag

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -1,3 +1,4 @@
+var view = require("ui/core/view");
 var frameModule = require("ui/frame");
 var NumberListModel = require("../../models/list");
 var application = require("application");
@@ -5,6 +6,15 @@ var Intent = android.content.Intent;
 var Uri = android.net.Uri;
 var operatorInfoModel = new NumberListModel();
 var pressedNumber;
+
+function setCallLabelClass(args, newClass) {
+    var sender = args.object;
+    var parent = sender.parent;
+    if (parent) {
+        var lbl = view.getViewById(parent, "labelCall");
+        lbl.className = newClass;
+    }
+}
 
 exports.loaded = function(args) {
     var page = args.object;
@@ -17,7 +27,7 @@ exports.loaded = function(args) {
     }
 };
 
-exports.tapCall = function() {
+exports.tapCall = function(e) {
     if (pressedNumber !== undefined) {
         var number = pressedNumber.text.replace(/\D/g, "");
         var dial = new Intent(Intent.ACTION_DIAL);
@@ -26,6 +36,7 @@ exports.tapCall = function() {
         application.android.foregroundActivity.startActivity(dial);
         pressedNumber.className = "";
         pressedNumber = undefined;
+        setCallLabelClass(e, "call-btn");
     }
 };
 
@@ -35,6 +46,7 @@ exports.listViewItemTap = function(e) {
     console.log(e.view.text);
     e.view.className = "pressed";
     pressedNumber = e.view;
+    setCallLabelClass(e, "call-active");
 };
 
 exports.tapBackToMain = function() {
