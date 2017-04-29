580dd2654cbc0e3d3c9cec511f206c92448b33fc
envy124 <aspidfire9664@gmail.com>
2017-04-29 11:33:28 +0300

    number pressed

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -1,13 +1,21 @@
 var frameModule = require("ui/frame");
 var NumberListModel = require("../../models/list");
 var operatorInfoModel = new NumberListModel();
-
+var pressedNumber;
 
 exports.loaded = function(args) {
     var page = args.object;
     var context = page.navigationContext;
     operatorInfoModel.switchTo(context.operatorName);
     page.bindingContext = operatorInfoModel;
+};
+
+exports.listViewItemTap = function(e) {
+    if (pressedNumber !== undefined) 
+        pressedNumber.className = "";
+    console.log(e.view.text);
+    e.view.className = "pressed";
+    pressedNumber = e.view;
 };
 
 exports.tapBackToMain = function() {
